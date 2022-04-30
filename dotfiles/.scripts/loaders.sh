16ffd3514574cc1fe4d70f6d37c461cde156c0f4
Francisco Figari <figafran@gmail.com>
2022-04-30 15:55:51 -0300

    Revert change in loader

--- dotfiles/.scripts/loaders.sh
+++ dotfiles/.scripts/loaders.sh
@@ -3,7 +3,8 @@ load-node () {
   [ -z node ] && unset -f node
   [ -z npm ] && unset -f npm
 
-  source /usr/share/nvm/init-nvm.sh
+  export NVM_DIR=~/.nvm
+  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
   [ -f .nvmrc ] && nvm use || true
 
   return 0
